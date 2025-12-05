ce28c8ca787958a2d6eade380e59f02cd9417b85
amin <aminsust11@gmail.com>
2025-12-05 13:49:31 +0600

    update the file

--- eduai-frontend/src/api/api.jsx
+++ eduai-frontend/src/api/api.jsx
@@ -4,7 +4,6 @@ const API = axios.create({
   baseURL: "http://localhost:3000/api", // change to your backend
 });
 
-// Add token to headers if exists
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
